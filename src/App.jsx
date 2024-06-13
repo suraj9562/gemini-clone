3fe39e701b4c7942dc95a3bdca4d2269cd57fb19
@suraj05 <wadikarsuraj02@gmail.com>
2024-06-13 21:14:57 +0530

    Done with basic set-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
 };
 
 export default App;
